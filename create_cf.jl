1d0c9ef8cd755633a6e2963c7a03dd66781a0a8e
Zhaoxing Wu <zhaoxingwu@Zhaoxings-MacBook-Pro.local>
2022-04-16 17:13:34 -0500

    correct cf tables

--- create_cf.jl
+++ create_cf.jl
@@ -1,19 +1,36 @@
 using CSV, DataFrames
-# cd("/Users/zhaoxingwu/Desktop/claudia lab/2022 spring phylogenetic/code")
+cd("/Users/zhaoxingwu/Desktop/claudia lab/2022 spring phylogenetic/code")
 
 function main()
+    println("---------------------------------------------------")
+    #19 different n, 51 different a
+    n_order = [[0, 0, 2, 2], [0, 1, 2, 1], [0, 1, 1, 2], [0, 2, 2, 0], 
+    [0, 2, 1, 1], [0, 2, 0, 2], [1, 0, 2, 1], [1, 0, 1, 2], 
+    [1, 1, 2, 0], [1, 1, 1, 1], [1, 1, 0, 2], [1, 2, 1, 0], 
+    [1, 2, 0, 1], [2, 0, 2, 0], [2, 0, 1, 1], [2, 0, 0, 2], 
+    [2, 1, 1, 0], [2, 1, 0, 1], [2, 2, 0, 0]]
+
     cf = CSV.read("N2222_expCF.txt", DataFrame)
+    #N = [("A", "B"), ("C", "D"), ("E", "F"), ("G", "H")]
+    N = [("A", "D"), ("C", "B"), ("E", "F"), ("G", "H")]
+    rst = get_a(cf, N, n_order)
+    for i in 1:Int(length(rst)/3)
+        println(rst[i*3-2], " ", rst[i*3-1], " ", rst[i*3])
+    end
+
     for i in 1:70
         temp = Array(cf[i, :])
         q = temp[1:4]
         cfs = temp[5:7]
-        n = get_n([("A", "B"), ("C", "D"), ("E", "F"), ("G", "H")], q)
+        n = get_n(N, q)
+        #println(n)
         if n != [1, 1, 1, 1]
-            #print(q)
-            #println(cfs_in_order([("A", "B"), ("C", "D"), ("E", "F"), ("G", "H")], q, cfs))
+            #println(cfs_in_order(N, q, cfs)[1], ", ", cfs_in_order(N, q, cfs)[2], ", ", cfs_in_order(N, q, cfs)[3])
+        else
+            #println(cfs[1], ", ", cfs[2], ", ", cfs[3])
         end
     end
-    get_a(cf, [("A", "B"), ("C", "D"), ("E", "F"), ("G", "H")])
+    #get_a(cf, N)
 end
 
 """
@@ -88,47 +105,38 @@ end
 
 """
     input:
-        df: dataframe of concordance factors
+        cf: dataframe of concordance factors
         N: array of tuples, individuals from each triangles of the network
     output: returns vector a
         for a's with the same quartet structure `n`, take the mean of a's
         the order of a is determined by the order of its corresponding quartet structure in the dataframe
 
 """
-function get_a(df, N)
-    a = []
-    c = [] # create vector that counts how many rows are mapped to each a value
-    n_arr = [] # keep track of what quartet structure a stands for
-    for i in 1:70
+function get_a(cf, N, n_order)
+    a = zeros(length(n_order)*3)
+    c = zeros(length(n_order)) #create vector that counts how many rows are mapped to each a value
+    for i in 1:nrow(cf)
         temp = Array(cf[i, :])
         q = temp[1:4]
         cfs = [temp[5], temp[6], temp[7]]
         n = get_n(N, q)
         if n != [1, 1, 1, 1] #skip it for now
             cfs_ord = cfs_in_order(N, q, cfs)
-            ind = findall(x->x==n, n_arr) # check if the same quartet structure has been visited before
-            if size(ind) != (0,) # if the same quarter structure has been found
-                a[ind[1]] += cfs
-                c[ind[1]] += 1
-            else # if not found
-                push!(a, cfs)
-                push!(c, 1)
-                push!(n_arr, n)
-            end
+            ind = findall(x->x==n, n_order)[1]
+            a[ind*3] += cfs_ord[3]
+            a[ind*3-1] += cfs_ord[2]
+            a[ind*3-2] += cfs_ord[1]
+            c[ind] += 1
         end
     end
 
     for i in 1:length(c)
-        a[i] = a[i]/c[i] # take the mean of a
+        a[i*3] = a[i*3]/c[i]
+        a[i*3-1] = a[i*3-1]/c[i]
+        a[i*3-2] = a[i*3-2]/c[i]
     end
 
-    ret = []
-    for i in a 
-        push!(ret, i[1])
-        push!(ret, i[2])
-        push!(ret, i[3])
-    end
-    return ret
+    return a
 end
 
 main()
