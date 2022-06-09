a8970eaaa0e178574e4f828f7f4b7f9edc941822
Zhaoxing Wu <zhaoxingwu@Zhaoxings-MacBook-Pro.local>
2022-06-09 11:34:08 -0500

    modified into a function

--- test/test_invariants.jl
+++ test/test_invariants.jl
@@ -9,78 +9,92 @@ using DataFrames
 using CSV
 
 ## 1. Choose a network in N vector format
-N = [("A","E"),("C","B"),("D","F"),("G","H")]
-
 ## 2. Write the network in parenthetical format
-## For this step, you need to draw the network by hand (rooted)
-## and write the parenthetical format based on the wiki rules
-network = "((((A,B))#H1:::0.8,((E,F),(#H1:::0.2,(C,D)))),(G,H));"
-
-
+    ## For this step, you need to draw the network by hand (rooted)
+    ## and write the parenthetical format based on the wiki rules
 ## 3. Read network in parenthetical format
-net = readTopology(network)
-plot(net,:R) ## just to check it matches your drawing
-
-
 ## 4. Create table of CF values
-quartets = PhyloNetworks.allQuartets(tipLabels(net), false)
-tx1 = []
-tx2 = []
-tx3 = []
-tx4 = []
+## 5. Get the vector of a
+## 6. Evaluating invariants on the true networks
+    ## So, we expect numbers to be close to zero
+    ## With the true network having the smallest value
+## 7. Test a wrong network to see that invariants are not close to zero
 
-for q in quartets
-    push!(tx1,q.taxon[1])
-    push!(tx2,q.taxon[2])
-    push!(tx3,q.taxon[3])
-    push!(tx4,q.taxon[4])
+function main()
+    #N = [("A","B"),("C","D"),("E","F"),("G","H")]
+    #network = "((((A,B))#H1:::0.8,((E,F),(#H1:::0.2,(C,D)))),(G,H));"
+    N = [("A", NaN),("C","D"),("E","F"),("G","H")]
+    network = "(((A)#H1:::0.8,((E,F),(#H1:::0.2,(C,D)))),(G,H));"
+    cf = generate_cf(N, network)
+    println(test_invariants(N, cf))
+
+    N = [("A","E"),("C","B"),("D","F"),("G","H")] #test a wrong network
+    println(test_invariants(N, cf))
 end
 
-df = DataFrame(tx1=tx1, tx2=tx2, tx3=tx3, tx4=tx4, obsCF1=ones(length(tx1)), obsCF2=zeros(length(tx1)), obsCF3=zeros(length(tx1)))
-cf = readTableCF!(df)
+"""
+    input:
+        N: array of tuples, individuals from each triangles of the network
+        network: the parenthetical format based on the wiki rules
+    output: the table of CF values
+"""
+function generate_cf(N, network)
+    net = readTopology(network)
+    plot(net,:R) ## just to check it matches your drawing
+    
+    #identifiable edges lengths were originally missing, so assigned default value of 1.0
+    quartets = PhyloNetworks.allQuartets(tipLabels(net), false) 
+    tx1 = []
+    tx2 = []
+    tx3 = []
+    tx4 = []
 
-## We need to optimize the pseudolikelihood
-## on the network for those dummy observed CF
-## in order to compute the expected CFs
-topologyQPseudolik!(net, cf)
-df_wide = fittedQuartetCF(cf)
+    for q in quartets
+        push!(tx1,q.taxon[1])
+        push!(tx2,q.taxon[2])
+        push!(tx3,q.taxon[3])
+        push!(tx4,q.taxon[4])
+    end
 
-## "true" concordance factors:
-cf = df_wide[:,[:tx1, :tx2, :tx3, :tx4, :expCF12, :expCF13, :expCF14]]
+    df = DataFrame(tx1=tx1, tx2=tx2, tx3=tx3, tx4=tx4, obsCF1=ones(length(tx1)), obsCF2=zeros(length(tx1)), obsCF3=zeros(length(tx1)))
+    cf = readTableCF!(df)
 
-## 5. Get the vector of a
-a = get_a(cf, N)
+    ## We need to optimize the pseudolikelihood on the network for those dummy observed CF in order to compute the expected CFs
+    topologyQPseudolik!(net, cf)
+    df_wide = fittedQuartetCF(cf)
 
-## 6. Evaluating invariants on the true networks
-## So, we expect numbers to be close to zero
-## With the true network having the smallest value
-norm(inv_net1112(a))
-norm(inv_net1121(a))
-norm(inv_net1211(a))
-norm(inv_net2111(a))
-norm(inv_net1122(a))
-norm(inv_net1212(a))
-norm(inv_net2112(a))
-norm(inv_net2211(a))
-norm(inv_net2121(a))
-norm(inv_net1221(a))
-norm(inv_net1222(a))
-norm(inv_net2212(a))
+    ## "true" concordance factors:
+    cf = df_wide[:,[:tx1, :tx2, :tx3, :tx4, :expCF12, :expCF13, :expCF14]]
+    #println(size(cf, 1))
+    return cf
+end
 
-## 7. Test a wrong network to see that invariants
-## are not close to zero
-N = [("A","E"),("C","B"),("D","F"),("G","H")]
-a = get_a(cf, N)
+"""
+    input:
+        N: array of tuples, individuals from each triangles of the network
+        cf: the expected CF table
+    output: an array of norm of invariants (we expect the value to be small if it is the correct network)
+"""
+function test_invariants(N, cf)
+    a = get_a(cf, N)
+    """
+    for i in 1:length(a)
+        print(i,": ")
+        println(a[i])
+    end
+    """
+    return [norm(inv_net1112(a)), 
+    norm(inv_net1121(a)),
+    norm(inv_net1211(a)),
+    norm(inv_net2111(a)),
+    norm(inv_net1122(a)),
+    norm(inv_net1212(a)),
+    norm(inv_net2112(a)),
+    norm(inv_net2211(a)),
+    norm(inv_net2121(a)),
+    norm(inv_net1221(a)),
+    norm(inv_net1222(a)),
+    norm(inv_net2212(a))]
+end
 
-norm(inv_net1112(a))
-norm(inv_net1121(a))
-norm(inv_net1211(a))
-norm(inv_net2111(a))
-norm(inv_net1122(a))
-norm(inv_net1212(a))
-norm(inv_net2112(a))
-norm(inv_net2211(a))
-norm(inv_net2121(a))
-norm(inv_net1221(a))
-norm(inv_net1222(a))
-norm(inv_net2212(a))
+main()
