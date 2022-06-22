7a1e7f48a01017392bf1f03b4b40537efb2d7561
Zhaoxing Wu <zhaoxingwu@Zhaoxings-MacBook-Pro.local>
2022-06-22 09:30:33 -0500

    added more tests

--- test/test_invariants.jl
+++ test/test_invariants.jl
@@ -6,7 +6,7 @@
 using PhyloNetworks
 using PhyloPlots
 using DataFrames
-using CSV
+using CSV, Statistics
 
 ## 1. Choose a network in N vector format
 ## 2. Write the network in parenthetical format
@@ -20,19 +20,88 @@ using CSV
     ## With the true network having the smallest value
 ## 7. Test a wrong network to see that invariants are not close to zero
 
-"""
-function main()
-    #N = [("A","B"),("C","D"),("E","F"),("G","H")]
-    #network = "((((A,B))#H1:::0.8,((E,F),(#H1:::0.2,(C,D)))),(G,H));"
-    N = [("A", NaN),("C","D"),("E","F"),("G","H")]
-    network = "(((A)#H1:::0.8,((E,F),(#H1:::0.2,(C,D)))),(G,H));"
-    cf = generate_cf(N, network)
-    println(test_invariants(N, cf))
 
-    N = [("A","E"),("C","B"),("D","F"),("G","H")] #test a wrong network
-    println(test_invariants(N, cf))
+function main()
+    """
+    t = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    N = [("A", "B"), ("C", "D"), ("E", "F"), ("G", "H")]
+    network = "(((A,B)#H1:::0.8, ((#H1:::0.2,(E,F)),(G,H))),(C,D));"
+    df = test_all_possible_nw(t, N, network)
+    CSV.write("network2222.csv",  df)
+    
+    t = ["A", "B", "C", "D", "E", "F", "G"]
+    N = [("A", "B"), ("C", "D"), ("E", "F"), ("G", NaN)]
+    network = "(((A,B)#H1:::0.8, ((#H1:::0.2,(E,F)),(G))),(C,D));"
+    df = test_all_possible_nw(t, network)
+    CSV.write("network2221.csv",  df)
+    
+    t = ["A", "B", "C", "D", "E", "F", "G"]
+    N = [("A", "B"), ("C", "D"), ("E", NaN), ("G", "F")]
+    network = "(((A,B)#H1:::0.8, ((#H1:::0.2,(E)),(G,F))),(C,D));"
+    df = test_all_possible_nw(t, network)
+    CSV.write(".\\result\\network2212.csv",  df)
+    
+    t = ["A", "B", "C", "D", "E", "F", "G"]
+    N = [("A", "B"), ("C", NaN), ("E", "F"), ("G", "D")]
+    network = "(((A,B)#H1:::0.8, ((#H1:::0.2,(E,F)),(G,D))),(C));"
+    df = test_all_possible_nw(t, network)
+    CSV.write("./result/network2122.csv",  df)
+
+    t = ["A", "B", "C", "D", "E", "F", "G"]
+    N = [("A", NaN), ("C", "D"), ("E", "F"), ("G", "B")]
+    network = "(((A)#H1:::0.8, ((#H1:::0.2,(E,F)),(G,B))),(C,D));"
+    df = test_all_possible_nw(t, network)
+    CSV.write("./result/network1222.csv",  df)
+    
+    t = ["A", "B", "C", "D", "E", "F"]
+    N = [("A", "B"), ("C", "D"), ("E", NaN), ("F", NaN)]
+    network = "(((A,B)#H1:::0.8, ((#H1:::0.2,(E)),(F))),(C,D));"
+    df = test_all_possible_nw(t, network)
+    CSV.write("./result/network2211.csv",  df)
+
+    t = ["A", "B", "C", "D", "E", "F"]
+    N = [("A", "B"), ("C", NaN), ("E", "D"), ("F", NaN)]
+    network = "(((A,B)#H1:::0.8, ((#H1:::0.2,(E,D)),(F))),(C));"
+    df = test_all_possible_nw(t, network)
+    CSV.write("./result/network2121.csv",  df)
+
+    t = ["A", "B", "C", "D", "E", "F"]
+    N = [("A", "B"), ("C", NaN), ("E", NaN), ("F", "D")]
+    network = "(((A,B)#H1:::0.8, ((#H1:::0.2,(E)),(F, D))),(C));"
+    df = test_all_possible_nw(t, network)
+    CSV.write("./result/network2112.csv",  df)
+
+    t = ["A", "B", "C", "D", "E", "F"]
+    N = [("A", NaN), ("C", "D"), ("E", "F"), ("B", NaN)]
+    network = "(((A)#H1:::0.8, ((#H1:::0.2,(E,F)),(B))),(C,D));"
+    df = test_all_possible_nw(t, network)
+    CSV.write("./result/network1221.csv",  df)
+
+    t = ["A", "B", "C", "D", "E", "F"]
+    N = [("A", NaN), ("C", "D"), ("B", NaN), ("E", "F")]
+    network = "(((A)#H1:::0.8, ((#H1:::0.2,(B)),(E,F))),(C,D));"
+    df = test_all_possible_nw(t, network)
+    CSV.write("./result/network1212.csv",  df)
+
+    t = ["A", "B", "C", "D", "E", "F"]
+    N = [("A", NaN), ("B", NaN), ("C", "D"), ("E", "F")]
+    network = "(((A)#H1:::0.8, ((#H1:::0.2,(C,D)),(E,F))),(B));"
+    df = test_all_possible_nw(t, network)
+    CSV.write("./result/network1122.csv",  df)
+
+    t = ["A", "B", "C", "D", "E", "F"]
+    N = [("A", NaN), ("B", NaN), ("C", "D"), ("E", "F")]
+    network = "(((A)#H1:::0.8, ((#H1:::0.2,(C,D)),(E,F))),(B));"
+    df = test_all_possible_nw(t, network)
+    CSV.write("./result/network1122.csv",  df)
+    """
+
+    t = ["A", "B", "C", "D", "E", "F"]
+    N = [("A", NaN), ("B", NaN), ("C", "D"), ("E", "F")]
+    network = "(((A)#H1:::0.8, ((#H1:::0.2,(C,D)),(E,F))),(B));"
+    df = test_all_possible_nw(t, network)
 end
-"""
+
 
 """
     input:
@@ -99,4 +168,39 @@ function test_invariants(N, cf)
     norm(inv_net2212(a))]
 end
 
-#main()
+"""
+input:
+    t: array of taxa ["A", "B", "C", "D", "E", "F", "G", "H"]
+    N: [("A", "B"), ("C", "D"), ("E", "F"), ("G", "H")]
+    network: newick format "(((A,B)#H1:::0.8, ((#H1:::0.2,(E,F)),(G,H))),(C,D));"
+output:
+    a dataframe of invariant values where each column is each possible network of n taxa, 
+    the last row is the average of all invariants
+    the true network is N
+"""
+function test_all_possible_nw(t, N, network)
+    net_all = list_nw(t)
+    cf = generate_cf(N, network)
+    println(cf)
+    df = DataFrame()
+    for i in 1:length(net_all)
+        colname = ""
+        for n in net_all[i]
+            for j in n
+                colname=string(colname, j)
+            end
+            colname=string(colname,",")
+        end
+        val = test_invariants(net_all[i], cf)
+        push!(val, mean(filter(!isnan, val)))
+        df[!,colname] = val
+    end
+    insertcols!(df, 1, :row => ["1112","1121","1211","2111","1122","1212","2112","2211","2121","1221","1222","2212", "mean"])
+    
+    inv_mean =Matrix(df)[end,:][2:end]
+    println(sort(inv_mean, rev=false)[1:5]) #print the top large invariant mean
+    println(net_all[sortperm(inv_mean)[1:5]]) #print the corresponding network structure
+    return df
+end
+
+main()
