3d3a3b7955956521108bd37792acccb09e9a43c7
Claudia Solis-Lemus <solislemus@wisc.edu>
2022-11-22 14:17:29 -0600

    changes to plots

--- scripts/julia/plot-dog-net.jl
+++ scripts/julia/plot-dog-net.jl
@@ -3,6 +3,15 @@
 
 using PhyloNetworks, PhyloPlots
 
+# Network by SNaQ and invariants
+
+net = readTopology("(((2,((6,4):0.4736595577884197,#H7:::0.2642622177134866):0.14802726306398745):1.5457473684659713,(5)#H7:::0.7357377822865134):2.4037173930657434,1,3);")
+
+
+
+
+# Other networks
+
 net = readTopology("((((((1,3),5),(2)#H1),6),#H1),4);")
 rootatnode!(net,"1")
 
